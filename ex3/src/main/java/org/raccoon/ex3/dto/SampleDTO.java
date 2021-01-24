93e561c20017f5569fc230e7e0b780270db0e098
raccoonjeong <rana3782@gmail.com>
2021-01-24 17:26:51 +0900

    ex3

---
+++ ex3/src/main/java/org/raccoon/ex3/dto/SampleDTO.java
@@ -0,0 +1,15 @@
+package org.raccoon.ex3.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
