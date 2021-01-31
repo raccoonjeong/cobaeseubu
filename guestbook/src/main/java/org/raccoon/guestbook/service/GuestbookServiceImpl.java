3caf3b6b67ebb5951d6028927c542c7abd714e50
raccoonjeong <rana3782@gmail.com>
2021-01-31 17:20:03 +0900

    Part2 4.8

--- guestbook/src/main/java/org/raccoon/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/raccoon/guestbook/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -44,4 +45,14 @@ public class GuestbookServiceImpl implements GuestbookService {
 
         return new PageResultDTO<>(result, fn);
     }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.isPresent() ? entityToDto(result.get()) : null;
+    }
+
+
 }
--- guestbook/src/test/java/org/raccoon/guestbook/service/GuestbookServiceTest.java
+++ guestbook/src/test/java/org/raccoon/guestbook/service/GuestbookServiceTest.java
@@ -3,6 +3,8 @@ package org.raccoon.guestbook.service;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.raccoon.guestbook.dto.GuestbookDTO;
+import org.raccoon.guestbook.dto.PageRequestDTO;
+import org.raccoon.guestbook.dto.PageResultDTO;
 import org.raccoon.guestbook.entity.Guestbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +25,26 @@ public class GuestbookServiceTest {
                 .writer("user0")
                 .build();
         System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("============================================");
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("--------------------------------------------");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+
     }
 
 }
