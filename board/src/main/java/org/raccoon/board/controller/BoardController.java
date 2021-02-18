1848da5dd8b20024be1dd7fb3c2dbbb02f3086a8
raccoonjeong <rana3782@gmail.com>
2021-02-19 01:25:04 +0900

    Part3 ~294p

--- board/src/main/java/org/raccoon/board/controller/BoardController.java
+++ board/src/main/java/org/raccoon/board/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.raccoon.board.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -41,6 +42,42 @@ public class BoardController {
         redirectAttributes.addFlashAttribute("msg", bno);
 
         return "redirect:/board/list";
+    }
+
+    @GetMapping({"/read", "/modify"})
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO,
+                     Long bno, Model model) {
+        log.info("bno : " + bno);
+        BoardDTO boardDTO = boardService.get(bno);
+        log.info(boardDTO);
+        model.addAttribute("dto", boardDTO);
+
+    }
+
+    @PostMapping("/remove")
+    public String remove(long bno, RedirectAttributes redirectAttributes) {
+        log.info("bno: " + bno);
+        boardService.removeWithReplies(bno);
+        redirectAttributes.addFlashAttribute("msg", bno);
+        return "redirect:/board/list";
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardDTO dto, @ModelAttribute("requestDTO") PageRequestDTO requestDTO,
+                         RedirectAttributes redirectAttributes) {
+        log.info("post modify....................................");
+        log.info("dto: " + dto);
+
+        boardService.modify(dto);
+
+        redirectAttributes.addAttribute("page", requestDTO.getPage());
+        redirectAttributes.addAttribute("type", requestDTO.getType());
+        redirectAttributes.addAttribute("keyword", requestDTO.getKeyword());
+
+        redirectAttributes.addAttribute("bno", dto.getBno());
+
+        return "redirect:/board/read";
+
     }
 
 }
